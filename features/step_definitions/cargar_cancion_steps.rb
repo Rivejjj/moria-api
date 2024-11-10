983b5dcdddf193ba543b68a5cb5eceb62dbc8e9e
Juan Pablo Aschieri <jaschieri@fi.uba.ar>
2024-11-10 17:58:34 -0300

    Definicion de US23:1 - Administrador da de alta una cancion

---
+++ features/step_definitions/cargar_cancion_steps.rb
@@ -0,0 +1,20 @@
+Dado('que no hay canciones registradas') do
+  repo_contenido = RepositorioContenido.new
+  repo_contenido.delete_all
+end
+
+Cuando('el administrador carga la cancion {string} hecha por {string} en {int} con duracion de {int} segundos y genero {string}') do |nombre, autor, anio, duracion, genero|
+  @request_body = { nombre:, autor:, anio:, duracion:, genero: }
+  @response = Faraday.post('/canciones', @request_body.to_json, { 'Content-Type' => 'application/json' })
+end
+
+Entonces('se da de alta la cancion') do
+  expect(@response.status).to eq(201)
+  repo_contenido = RepositorioContenido.new
+  cancion = repo_contenido.first
+  expect(cancion.nombre).to eq(@request_body[:nombre])
+  expect(cancion.autor).to eq(@request_body[:autor])
+  expect(cancion.anio).to eq(@request_body[:anio])
+  expect(cancion.duracion).to eq(@request_body[:duracion])
+  expect(cancion.genero).to eq(@request_body[:genero])
+end
