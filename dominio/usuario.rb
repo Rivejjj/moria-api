ae8ee74a7611e5d03561bb1b46be6616917db0f8
IvanBotoshansky <ibotoshansky@fi.uba.ar>
2024-11-11 18:58:47 -0300

    Usuario puede agregar una cancion a su playlist

--- dominio/usuario.rb
+++ dominio/usuario.rb
@@ -1,5 +1,5 @@
 class Usuario
-  attr_reader :nombre, :email, :id_plataforma, :updated_on, :created_on
+  attr_reader :nombre, :email, :id_plataforma, :playlist, :updated_on, :created_on
   attr_accessor :id
 
   def initialize(nombre, email, id_plataforma, id = nil)
@@ -7,5 +7,10 @@ class Usuario
     @nombre = nombre
     @email = email
     @id_plataforma = id_plataforma
+    @playlist = []
+  end
+
+  def agregar_a_playlist(contenido)
+    @playlist << contenido
   end
 end
--- spec/dominio/usuario_spec.rb
+++ spec/dominio/usuario_spec.rb
@@ -8,4 +8,11 @@ describe Usuario do
     expect(usuario.email).to eq('email@email.com')
     expect(usuario.id_plataforma).to eq(141_733_544)
   end
+
+  it 'puede agregar una cancion a su playlist' do
+    usuario = described_class.new('nombre', 'email@email.com', 141_733_544)
+    cancion = instance_double('Cancion', nombre: 'cancion')
+    usuario.agregar_a_playlist(cancion)
+    expect(usuario.playlist).to include(cancion)
+  end
 end
