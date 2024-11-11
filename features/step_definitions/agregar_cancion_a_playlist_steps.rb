0c9c0ff03f669de66e780d2057fa1e5ecc279d62
IvanBotoshansky <ibotoshansky@fi.uba.ar>
2024-11-11 18:40:08 -0300

    Escenario: US16:1 - Usuario agrega una cancion a su playlist en progreso

---
+++ features/step_definitions/agregar_cancion_a_playlist_steps.rb
@@ -0,0 +1,18 @@
+Dado('existe una cancion {string} con id {int}') do |nombre_cancion, id_cancion|
+  repo_contenido = RepositorioContenido.new
+  info_cancion = InformacionCancion.new(nombre_cancion, 'autor', 2020, 180, 'rock')
+  cancion = Cancion.new(info_cancion, id_cancion)
+  repo_contenido.save(cancion)
+end
+
+Cuando('el usuario agrega la cancion con id {int} a su playlist') do |id_cancion|
+  request_body = { 'id_cancion' => id_cancion }.to_json
+  @response = Faraday.post("/usuarios/#{ID_PLATAFORMA_PRUEBA}/playlist", request_body, { 'Content-Type' => 'application/json' })
+end
+
+Entonces('se agrega la cancion {string} a la playlist') do |nombre_cancion|
+  expect(@response.status).to eq(201)
+  repo_usuarios = RepositorioUsuarios.new
+  usuario = repo_usuarios.find_by_id_plataforma(ID_PLATAFORMA_PRUEBA)
+  expect(usuario.tiene_cancion_en_playlist(nombre_cancion)).to eq true
+end
