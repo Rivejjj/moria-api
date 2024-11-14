9bc7f0cb35b6a0a556ff79a3081ca7430916cae7
Alan Botoshansky <abotoshansky@fi.uba.ar>
2024-11-14 17:56:00 -0300

    Podcast se puede obtener el nombre del podcast

--- dominio/podcast.rb
+++ dominio/podcast.rb
@@ -2,4 +2,8 @@ class Podcast
   def initialize(info_podcast)
     @info_podcast = info_podcast
   end
+
+  def nombre
+    @info_podcast.nombre
+  end
 end
--- spec/dominio/podcast_spec.rb
+++ spec/dominio/podcast_spec.rb
@@ -8,4 +8,10 @@ describe Podcast do
     podcast = described_class.new(info_podcast)
     expect(podcast).not_to be_nil
   end
+
+  it 'se puede obtener el nombre del podcast' do
+    info_podcast = InformacionContenido.new('nombre', 'autor', 2021, 180, 'rock')
+    podcast = described_class.new(info_podcast)
+    expect(podcast.nombre).to eq(info_podcast.nombre)
+  end
 end
