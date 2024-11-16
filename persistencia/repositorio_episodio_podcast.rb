4ddc05dbc2cd1c65972da989abe8c9a1f0a7242a
Joaquin <jorivero@fi.uba.ar>
2024-11-16 18:02:50 -0300

    repositorio podcast deberia devoler error al no encontrar el podcast para el episodio

--- persistencia/repositorio_episodio_podcast.rb
+++ persistencia/repositorio_episodio_podcast.rb
@@ -1,6 +1,15 @@
+require_relative 'repositorio_contenido'
+
 class RepositorioEpisodiosPodcast < AbstractRepository
   self.table_name = :episodios_podcast
   self.model_class = 'EpisodioPodcast'
+
+  def save(episodio)
+    repo_contenido = RepositorioContenido.new
+    raise ContenidoNoEncontradoError if repo_contenido.find(episodio.id_podcast).nil?
+
+    super(episodio)
+  end
 
   def load_object(a_hash)
     EpisodioPodcast.new(a_hash[:numero_episodio], a_hash[:id_podcast], a_hash[:nombre], a_hash[:duracion], a_hash[:id])
--- spec/persistencia/repositorio_episodios_podcast_spec.rb
+++ spec/persistencia/repositorio_episodios_podcast_spec.rb
@@ -3,9 +3,22 @@ require_relative '../../dominio/episodio_podcast'
 require_relative '../../persistencia/repositorio_episodio_podcast'
 
 describe RepositorioEpisodiosPodcast do
+  before(:each) do
+    RepositorioContenido.new.delete_all
+  end
+
   it 'deberia guardar y asignar id a un episodio podcast' do
+    info_contenido = InformacionContenido.new('nombre', 'autor', 2021, 180, 'genero')
+    podcast = Podcast.new(info_contenido, 1)
+    RepositorioContenido.new.save(podcast)
+
     episodio_podcast = EpisodioPodcast.new(1, 1, 'nombre', 180)
     described_class.new.save(episodio_podcast)
     expect(episodio_podcast.id).not_to be_nil
+  end
+
+  it 'deberia devoler error al no encontrar el podcast para el episodio' do
+    episodio_podcast = EpisodioPodcast.new(1, 1, 'nombre', 180)
+    expect { described_class.new.save(episodio_podcast) }.to raise_error(ContenidoNoEncontradoError)
   end
 end
