b7e35183cf561f973d9e859636f56728357e5d06
Joaquin <jorivero@fi.uba.ar>
2024-11-16 20:36:03 -0300

    comienzo Característica: Dar me gusta a cancion

---
+++ features/registrar_reproduccion_episodio_podcast.feature
@@ -0,0 +1,8 @@
+# language: es
+Característica: Cargar reproduccion de episodio de podcast
+  @wip
+  Escenario: US40:1 - Registracion de reproduccion de un episodio de un podcast para un usuario existente
+      Dado que existe un usuario "Kevin"
+      Y existe un episodio de un podcast con id 1
+      Cuando reproduce un episodio de un podcast con id 1
+      Entonces se registra la reproduccion del episodio del podcast
---
+++ features/step_definitions/registrar_reproduccion_episodio_podcast_steps.rb
@@ -0,0 +1,22 @@
+Dado('existe un episodio de un podcast con id {int}') do |id|
+  episodios_repo = RepositorioEpisodiosPodcast.new
+  contenido_repo = RepositorioContenido.new
+  podcast = Podcast.new(InformacionContenido.new('nombre', 'autor', 2021, 180, 'genero'))
+  contenido_repo.save(podcast)
+
+  episodio_podcast = EpisodioPodcast.new(1, podcast.id, 'nombre', 4567, id)
+  episodios_repo.save(episodio_podcast)
+end
+
+Cuando('reproduce un episodio de un podcast con id {int}') do |id_episodio|
+  @request_body = { id_plataforma: @usuario.id_plataforma }
+  @response = Faraday.post("/episodio_podcast/#{id_episodio}/reproduccion", @request_body.to_json, { 'Content-Type' => 'application/json' })
+  @id_episodio = id_episodio
+end
+
+Entonces('se registra la reproduccion del episodio del podcast') do
+  expect(@response.status).to eq(201)
+  repo_usuarios = RepositorioUsuarios.new
+  usuario = repo_usuarios.find_by_nombre(@usuario.nombre)
+  expect(usuario.reproducciones.map(&:id)).to include(@id_episodio)
+end
