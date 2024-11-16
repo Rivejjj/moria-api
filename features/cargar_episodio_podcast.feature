06682b2218a853ca30d2bb74a7b53b397ef20d1e
Joaquin <jorivero@fi.uba.ar>
2024-11-16 15:16:06 -0300

    empezando feature Cargar episodio de podcast

---
+++ features/cargar_episodio_podcast.feature
@@ -0,0 +1,16 @@
+#language: es
+Característica: Cargar episodio de podcast
+
+  @wip
+  Escenario: US39:1 - Administrador carga un episodio de un podcast
+    Dado que no hay episodios de podcasts registrados
+    Y existe un podcast con id 1
+    Cuando el administrador carga el episodio numero 1 de un podcast con id 1 llamado "Episodio 1" con duracion de 3600 segundos
+    Entonces se da de alta el episodio del podcast
+
+  @wip
+  Escenario: US39:2 - Administrador carga un episodio de un podcast que no existe
+    Dado que no hay episodios de podcasts registrados
+    Y que no hay podcasts registrados
+    Cuando el administrador carga el episodio numero 1 de un podcast con id 1 llamado "Episodio 1" con duracion de 3600 segundos
+    Entonces no se da de alta el episodio del podcast
---
+++ features/step_definitions/cargar_episodio_podcast_steps.rb
@@ -0,0 +1,31 @@
+Dado('que no hay episodios de podcasts registrados') do
+  repo_episodios = RepositorioEpisodiosPodcast.new
+  repo_episodios.delete_all
+end
+
+Dado('existe un podcast con id {int}') do |id|
+  repo_contenido = RepositorioContenido.new
+  info_podcast = InformacionContenido.new('El podcast de la semana', 'Juan Perez', 2020, 36_000, 'Entretenimiento')
+  podcast = Podcast.new(info_podcast, id)
+  repo_contenido.save(podcast)
+end
+
+Cuando('el administrador carga el episodio numero {int} de un podcast con id {int} llamado {string} con duracion de {int} segundos') do |numero, id_podcast, nombre, duracion|
+  @request_body = { nombre:, numero:, duracion: }
+  @response = Faraday.post("/podcasts/#{id_podcast}/episodios", @request_body.to_json, { 'Content-Type' => 'application/json' })
+end
+
+Entonces('se da de alta el episodio del podcast') do
+  expect(@response.status).to eq(201)
+  repo_episodios = RepositorioEpisodiosPodcast.new
+  episodio = repo_episodios.first
+  expect(episodio.nombre).to eq(@request_body[:nombre])
+  expect(episodio.numero).to eq(@request_body[:numero])
+  expect(episodio.duracion).to eq(@request_body[:duracion])
+end
+
+Entonces('no se da de alta el episodio del podcast') do
+  expect(@response.status).to eq(404)
+  repo_episodios = RepositorioEpisodiosPodcast.new
+  expect(repo_episodios.all).to be_empty
+end
