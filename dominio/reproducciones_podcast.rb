efc3cb34f768a0967e1aaf6b9ed616be3b2bd936
Juan Pablo Aschieri <jaschieri@fi.uba.ar>
2024-11-18 15:51:11 -0300

    ReproduccionesPodcast contiene_reproduccion_de? deberia devolver false si no contiene al menos 2 reproducciones del usuario de episodios

--- dominio/reproducciones_podcast.rb
+++ dominio/reproducciones_podcast.rb
@@ -1,10 +1,15 @@
 class ReproduccionesPodcast
+  CANTIDAD_MINIMA_DE_EPISODIOS_REPRODUCIDOS = 2
+
   def initialize(podcast, reproducciones_episodios)
     @podcast = podcast
     @reproducciones_episodios = reproducciones_episodios
   end
 
-  def contiene_reproduccion_de?(_usuario)
-    true
+  def contiene_reproduccion_de?(usuario)
+    cantidad_reproducidos = @reproducciones_episodios.count do |reproducciones_episodio|
+      reproducciones_episodio.contiene_reproduccion_de?(usuario)
+    end
+    cantidad_reproducidos >= CANTIDAD_MINIMA_DE_EPISODIOS_REPRODUCIDOS
   end
 end
--- spec/dominio/reproducciones_podcast_spec.rb
+++ spec/dominio/reproducciones_podcast_spec.rb
@@ -3,14 +3,22 @@ require_relative '../../dominio/reproducciones_podcast'
 
 describe ReproduccionesPodcast do
   describe 'contiene_reproduccion_de?' do
-    it 'deberia devolver true si contiene la reproduccion del usuario' do
+    it 'deberia devolver true si contiene al menos 2 reproducciones del usuario de episodios' do
       usuario = instance_double('Usuario', es_el_mismo_usuario_que?: true)
       podcast = instance_double('Podcast')
-      reprodcciones_episodio1 = instance_double('ReproduccionesEpisodioPodcast')
-      reproducciones_episodio2 = instance_double('ReproduccionesEpisodioPodcast')
+      reprodcciones_episodio1 = instance_double('ReproduccionesEpisodioPodcast', contiene_reproduccion_de?: true)
+      reproducciones_episodio2 = instance_double('ReproduccionesEpisodioPodcast', contiene_reproduccion_de?: true)
       reproducciones = described_class.new(podcast, [reprodcciones_episodio1, reproducciones_episodio2])
 
       expect(reproducciones.contiene_reproduccion_de?(usuario)).to be true
+    end
+
+    it 'deberia devolver false si no contiene al menos 2 reproducciones del usuario de episodios' do
+      usuario = instance_double('Usuario')
+      podcast = instance_double('Podcast')
+      reproducciones = described_class.new(podcast, [])
+
+      expect(reproducciones.contiene_reproduccion_de?(usuario)).to be false
     end
   end
 end
