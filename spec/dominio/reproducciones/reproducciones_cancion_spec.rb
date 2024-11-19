f35cd894224fc4461186fbeeab036d5d912ac3a7
Joaquin <jorivero@fi.uba.ar>
2024-11-18 23:39:55 -0300

    ReproduccionesCancion contiene_reproduccion_de? deberia devolver false si no contiene la reproduccion del usuario

--- spec/dominio/reproducciones/reproducciones_cancion_spec.rb
+++ spec/dominio/reproducciones/reproducciones_cancion_spec.rb
@@ -16,13 +16,21 @@ describe ReproduccionesCancion do
 
   describe 'contiene_reproduccion_de?' do
     it 'deberia devolver true si contiene la reproduccion del usuario' do
-      episodio = instance_double('Cancion')
-      reproducciones = described_class.new(episodio)
+      cancion = instance_double('Cancion')
+      reproducciones = described_class.new(cancion)
 
       usuario = instance_double('Usuario', es_el_mismo_usuario_que?: true)
       reproducciones.agregar_reproduccion_de(usuario)
 
       expect(reproducciones.contiene_reproduccion_de?(usuario)).to be true
+    end
+
+    it 'deberia devolver false si no contiene la reproduccion del usuario' do
+      cancion = instance_double('Cancion')
+      reproducciones = described_class.new(cancion)
+      usuario = instance_double('Usuario', es_el_mismo_usuario_que?: false)
+
+      expect(reproducciones.contiene_reproduccion_de?(usuario)).to be false
     end
   end
 end
