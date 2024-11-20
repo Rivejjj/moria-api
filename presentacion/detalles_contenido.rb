3aae1c360f9bdce6f197b415ca317a67d297c18c
Joaquin <jorivero@fi.uba.ar>
2024-11-19 23:48:11 -0300

    DetallesContenido obtener_json deberia obtener el json de los detalles de un podcast

--- presentacion/detalles_contenido.rb
+++ presentacion/detalles_contenido.rb
@@ -4,6 +4,38 @@ class DetallesContenido
   end
 
   def obtener_json
+    if @contenido.is_a? Cancion
+      puts 'entro a cancion'
+      obtener_json_cancion
+    elsif @contenido.is_a? Podcast
+      obtener_json_podcast
+    end
+  end
+
+  private
+
+  def obtener_json_podcast
+    { 'podcast': @contenido.id,
+      'detalles': {
+        'nombre': @contenido.nombre,
+        'autor': @contenido.autor,
+        'genero': @contenido.genero,
+        'episodios': obtener_json_episodios(@contenido.episodios)
+      } }.to_json
+  end
+
+  def obtener_json_episodios(episodios)
+    detalles_episodios = {}
+    episodios.each do |episodio|
+      detalles_episodios[episodio.numero_episodio.to_s] = {
+        'nombre': episodio.nombre,
+        'duracion': episodio.duracion
+      }
+    end
+    detalles_episodios
+  end
+
+  def obtener_json_cancion
     { 'cancion': @contenido.id,
       'detalles': {
         'nombre': @contenido.nombre,
--- spec/presentacion/detalles_contenido_spec.rb
+++ spec/presentacion/detalles_contenido_spec.rb
@@ -14,11 +14,37 @@ describe DetallesContenido do
       }
     }.to_json
 
+    podcast_json_esperado = {
+      'podcast': 1,
+      'detalles': {
+        'nombre': 'JRE',
+        'autor': 'Michael Jackson',
+        'genero': 'Cultura',
+        'episodios': {
+          '1': { 'nombre': 'Primer episodio', 'duracion': 916 },
+          '2': { 'nombre': 'Segundo episodio', 'duracion': 4368 }
+        }
+      }
+    }.to_json
+
     it 'deberia obtener el json de los detalles de una cancion' do
       cancion = instance_double(Cancion, nombre: 'Beat it', autor: 'Michael Jackson', anio: 1983, duracion: 378, genero: 'Pop', id: 1)
+      allow(cancion).to receive(:is_a?).with(Cancion).and_return(true)
       detalles_contenido = described_class.new(cancion)
 
       expect(detalles_contenido.obtener_json).to eq cancion_json_esperada
+    end
+
+    it 'deberia obtener el json de los detalles de un podcast' do
+      episodio1 = instance_double(EpisodioPodcast, nombre: 'Primer episodio', duracion: 916, numero_episodio: 1)
+      episodio2 = instance_double(EpisodioPodcast, nombre: 'Segundo episodio', duracion: 4368, numero_episodio: 2)
+      podcast = instance_double(Podcast, nombre: 'JRE', autor: 'Michael Jackson', genero: 'Cultura', episodios: [episodio1, episodio2], id: 1)
+
+      allow(podcast).to receive(:is_a?).with(Podcast).and_return(true)
+      allow(podcast).to receive(:is_a?).with(Cancion).and_return(false)
+
+      detalles_contenido = described_class.new(podcast)
+      expect(detalles_contenido.obtener_json).to eq podcast_json_esperado
     end
   end
 end
