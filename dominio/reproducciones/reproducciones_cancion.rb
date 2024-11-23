c42087b6d3d0a11f560c8d7a15969c118c1f0877
Joaquin <jorivero@fi.uba.ar>
2024-11-23 10:31:34 -0300

    ReproduccionesCancion reproducciones_de_la_semana deberia devolver las reproducciones de hasta una semana atras

--- dominio/reproducciones/reproducciones_cancion.rb
+++ dominio/reproducciones/reproducciones_cancion.rb
@@ -16,6 +16,14 @@ class ReproduccionesCancion < Reproducciones
   def assert_contiene_reproduccion_de(usuario)
     raise CancionNoReproducidaError unless contiene_reproduccion_de?(usuario)
   end
+
+  def reproducciones_de_la_semana(proveedor_de_fecha)
+    reproducciones_semana = @reproducciones.select { |reproduccion| reproduccion.fue_reproducido_hace_menos_de_una_semana?(proveedor_de_fecha) }
+
+    nuevo = ReproduccionesCancion.new(@cancion)
+    reproducciones_semana.each { |reproduccion| nuevo.agregar_reproduccion(reproduccion) }
+    nuevo
+  end
 end
 
 class CancionNoReproducidaError < StandardError; end
