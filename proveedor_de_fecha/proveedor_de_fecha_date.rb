c475ae7de82bf3c2ee3c912bde1e025ab4bf21e1
Juan Pablo Aschieri <jaschieri@fi.uba.ar>
2024-11-23 08:30:44 -0300

    ProveedorDeFechaDate en_la_ultima_semana? deberia devolver false si la fecha recibida esta fuera de la ultima semana

--- proveedor_de_fecha/proveedor_de_fecha_date.rb
+++ proveedor_de_fecha/proveedor_de_fecha_date.rb
@@ -1,5 +1,6 @@
 class ProveedorDeFechaDate
-  def en_la_ultima_semana?(_fecha)
-    true
+  DIAS_SEMANA = 7
+  def en_la_ultima_semana?(fecha)
+    fecha > Date.today - DIAS_SEMANA
   end
 end
--- spec/proveedor_de_fecha/proveedor_de_fecha_date_spec.rb
+++ spec/proveedor_de_fecha/proveedor_de_fecha_date_spec.rb
@@ -5,5 +5,9 @@ describe ProveedorDeFechaDate do
     it 'deberia devolver true si la fecha recibida esta dentro de la ultima semana' do
       expect(described_class.new.en_la_ultima_semana?(Date.today)).to eq true
     end
+
+    it 'deberia devolver false si la fecha recibida esta fuera de la ultima semana' do
+      expect(described_class.new.en_la_ultima_semana?(Date.today - 7)).to eq false
+    end
   end
 end
