ab64938c21bc822e799a1530df3b7be6f1b100ca
Juan Pablo Aschieri <jaschieri@fi.uba.ar>
2024-11-23 07:36:31 -0300

    ReproduccionesEpisodioPodcast reproducciones_de_la_semana deberia devolver las reproducciones de hasta una semana atras

--- dominio/reproducciones/reproducciones.rb
+++ dominio/reproducciones/reproducciones.rb
@@ -9,4 +9,12 @@ class Reproducciones
   def contiene_reproduccion_de?(_usuario)
     raise 'Subclass must implement'
   end
+
+  def reproducciones_de_la_semana(proveedor_de_fecha)
+    reproducciones_semana = @reproducciones.select { |reproduccion| reproduccion.fue_reproducido_hace_menos_de_una_semana?(proveedor_de_fecha) }
+
+    nuevo = self.class.new(@cancion)
+    reproducciones_semana.each { |reproduccion| nuevo.agregar_reproduccion(reproduccion) }
+    nuevo
+  end
 end
