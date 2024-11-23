6c086aba80ffd6a4aaf050bed55440828207633a
Joaquin <jorivero@fi.uba.ar>
2024-11-23 00:58:26 -0300

    Reproduccion reproducido_por? deberia devolver true si fue reproducido por el usuario

--- dominio/reproducciones/reproduccion.rb
+++ dominio/reproducciones/reproduccion.rb
@@ -6,6 +6,10 @@ class Reproduccion
     @fecha = fecha
   end
 
+  def reproducido_por?(usuario)
+    @usuario.es_el_mismo_usuario_que?(usuario)
+  end
+
   def fue_reproducido_hace_menos_de_una_semana?(proveedor_de_fecha)
     @fecha > proveedor_de_fecha.fecha_actual - DIAS_SEMANA
   end
--- spec/dominio/reproducciones/reproduccion_spec.rb
+++ spec/dominio/reproducciones/reproduccion_spec.rb
@@ -2,6 +2,14 @@ require 'spec_helper'
 require_relative '../../../dominio/reproducciones/reproduccion'
 
 describe Reproduccion do
+  describe 'reproducido_por?' do
+    it 'deberia devolver true si fue reproducido por el usuario' do
+      usuario = instance_double('Usuario', es_el_mismo_usuario_que?: true)
+      reproduccion = described_class.new(usuario, Date.new(2024, 11, 20))
+      expect(reproduccion.reproducido_por?(usuario)).to be true
+    end
+  end
+
   describe 'fue_reproducido_hace_menos_de_una_semana?' do
     it 'deberia devolver true si fue reproducido hace menos de una semana' do
       proveedor_de_fecha = instance_double('ProveedorDeFecha', fecha_actual: Date.new(2024, 11, 20))
