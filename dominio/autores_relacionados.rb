e7a796f127f363132b8ea82849aedfe15d73c6d1
IvanBotoshansky <ibotoshansky@fi.uba.ar>
2024-11-23 19:24:30 -0300

    AutoresRelacionados deberia devolver los autores relacionados

--- dominio/autores_relacionados.rb
+++ dominio/autores_relacionados.rb
@@ -1,7 +1,8 @@
 class AutoresRelacionados
-  attr_reader :autor
+  attr_reader :autor, :relacionados
 
-  def initialize(autor)
+  def initialize(autor, relacionados)
     @autor = autor
+    @relacionados = relacionados
   end
 end
--- spec/dominio/autores_relacionados_spec.rb
+++ spec/dominio/autores_relacionados_spec.rb
@@ -4,7 +4,15 @@ require_relative '../../dominio/autores_relacionados'
 describe AutoresRelacionados do
   it 'deberia devolver el autor a relacionar' do
     autor = instance_double('Autor')
-    autores_relacionados = described_class.new(autor)
+    relacionados = [instance_double('Autor1'), instance_double('Autor2'), instance_double('Autor3')]
+    autores_relacionados = described_class.new(autor, relacionados)
     expect(autores_relacionados.autor).to eq(autor)
+  end
+
+  it 'deberia devolver los autores relacionados' do
+    autor = instance_double('Autor')
+    relacionados = [instance_double('Autor1'), instance_double('Autor2'), instance_double('Autor3')]
+    autores_relacionados = described_class.new(autor, relacionados)
+    expect(autores_relacionados.relacionados).to eq(relacionados)
   end
 end
