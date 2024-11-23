5d8e5ff179109cdf1284424567a4bf245420037e
Juan Pablo Aschieri <jaschieri@fi.uba.ar>
2024-11-22 21:44:26 -0300

    Reproduccion fue_reproducido_hace_menos_de_una_semana? deberia devolver false si no fue reproducido hace menos de una semana

--- dominio/reproducciones/reproduccion.rb
+++ dominio/reproducciones/reproduccion.rb
@@ -1,10 +1,12 @@
 class Reproduccion
+  DIAS_SEMANA = 7
+
   def initialize(usuario, fecha)
     @usuario = usuario
     @fecha = fecha
   end
 
-  def fue_reproducido_hace_menos_de_una_semana?(_proveedor_de_fecha)
-    true
+  def fue_reproducido_hace_menos_de_una_semana?(proveedor_de_fecha)
+    @fecha > proveedor_de_fecha.fecha_actual - DIAS_SEMANA
   end
 end
--- spec/dominio/reproducciones/reproduccion_spec.rb
+++ spec/dominio/reproducciones/reproduccion_spec.rb
@@ -2,10 +2,19 @@ require 'spec_helper'
 require_relative '../../../dominio/reproducciones/reproduccion'
 
 describe Reproduccion do
-  it 'deberia devolver true si fue reproducido hace menos de una semana' do
-    proveedor_de_fecha = instance_double('ProveedorDeFecha', fecha_actual: Date.new(2024, 11, 20))
-    usuario = instance_double('Usuario')
-    reproduccion = described_class.new(usuario, Date.new(2024, 11, 20))
-    expect(reproduccion.fue_reproducido_hace_menos_de_una_semana?(proveedor_de_fecha)).to be true
+  describe 'fue_reproducido_hace_menos_de_una_semana?' do
+    it 'deberia devolver true si fue reproducido hace menos de una semana' do
+      proveedor_de_fecha = instance_double('ProveedorDeFecha', fecha_actual: Date.new(2024, 11, 20))
+      usuario = instance_double('Usuario')
+      reproduccion = described_class.new(usuario, Date.new(2024, 11, 20))
+      expect(reproduccion.fue_reproducido_hace_menos_de_una_semana?(proveedor_de_fecha)).to be true
+    end
+
+    it 'deberia devolver false si no fue reproducido hace menos de una semana' do
+      proveedor_de_fecha = instance_double('ProveedorDeFecha', fecha_actual: Date.new(2024, 11, 20))
+      usuario = instance_double('Usuario')
+      reproduccion = described_class.new(usuario, Date.new(2024, 11, 10))
+      expect(reproduccion.fue_reproducido_hace_menos_de_una_semana?(proveedor_de_fecha)).to be false
+    end
   end
 end
