8e311b146450578648d09cf99c2780afd4eefc2a
Joaquin <jorivero@fi.uba.ar>
2024-11-24 19:16:36 -0300

    RecomendadorDeContenido deberia recomendar una cancion del genero mas gustado

--- dominio/recomendador_de_contenido.rb
+++ dominio/recomendador_de_contenido.rb
@@ -1,7 +1,17 @@
 class RecomendadorDeContenido
   ULTIMAS_N_AGREGADAS = 5
 
+  def initialize(repositorio_contenido = nil)
+    @repositorio_contenido = repositorio_contenido
+  end
+
   def recomendar_contenido(usuario)
     usuario.playlist.last(ULTIMAS_N_AGREGADAS).reverse
+  end
+
+  def recomendar_cancion_de_genero_mas_gustado(me_gustas)
+    genero = me_gustas.genero_mas_gustado
+    canciones = @repositorio_contenido.get_canciones_de_genero(genero)
+    canciones.find { |cancion| !me_gustas.contenido_gustado?(cancion) }
   end
 end
--- spec/dominio/recomendador_de_contenido_spec.rb
+++ spec/dominio/recomendador_de_contenido_spec.rb
@@ -3,8 +3,28 @@ require_relative '../../dominio/recomendador_de_contenido'
 
 describe RecomendadorDeContenido do
   it 'deberia recomendar a un usuario los ultimos 5 contenidos agregados a su playlist' do
-    recomendador = described_class.new
+    recomendador = described_class.new(instance_double('RepositorioContenido'))
     usuario = instance_double('Usuario', playlist: %w[cancion1 cancion2 cancion3 cancion4 cancion5 cancion6])
     expect(recomendador.recomendar_contenido(usuario)).to eq(%w[cancion6 cancion5 cancion4 cancion3 cancion2])
   end
+
+  it 'deberia recomendar una cancion del genero mas gustado' do
+    cancion_rock1 = crear_mock_cancion
+    cancion_rock2 = crear_mock_cancion
+
+    repo_contenido = instance_double('RepositorioContenido', get_canciones_de_genero: [cancion_rock1, cancion_rock2])
+
+    me_gustas = MeGustasUsuario.new(instance_double('Usuario'), [cancion_rock1])
+
+    recomendador = described_class.new(repo_contenido)
+    cancion_recomendada = recomendador.recomendar_cancion_de_genero_mas_gustado(me_gustas)
+    expect(cancion_recomendada.genero).to eq('rock')
+    expect(cancion_recomendada).to eq(cancion_rock2)
+  end
+end
+
+def crear_mock_cancion
+  cancion_rock = instance_double('Cancion', genero: 'rock')
+  allow(cancion_rock).to receive(:es_el_mismo?) { |cancion| cancion == cancion_rock }
+  cancion_rock
 end
