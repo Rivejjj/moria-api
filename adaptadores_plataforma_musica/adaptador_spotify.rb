53d0eec78c159754fddac6ff838c7f47ee80a542
Alan Botoshansky <abotoshansky@fi.uba.ar>
2024-11-25 16:04:46 -0300

    Agregado de logs de requests enviadas a Spotify

--- adaptadores_plataforma_musica/adaptador_spotify.rb
+++ adaptadores_plataforma_musica/adaptador_spotify.rb
@@ -9,10 +9,10 @@ class AdaptadorSpotify
   GRANT_TYPE = 'client_credentials'.freeze
   SPOTIFY_AUTORES_API_URL = 'https://api.spotify.com/v1/artists'.freeze
 
-  def initialize(client_id = ENV['CLIENT_ID'], client_secret = ENV['CLIENT_SECRET'])
+  def initialize(client_id = ENV['CLIENT_ID'], client_secret = ENV['CLIENT_SECRET'], logger = SemanticLogger['AdaptadorSpotify'])
     @client_id = client_id
     @client_secret = client_secret
-    @logger = SemanticLogger['AdaptadorSpotify']
+    @logger = logger
   end
 
   def obtener_token
@@ -31,12 +31,11 @@ class AdaptadorSpotify
   def obtener_autores_relacionados_a(autor)
     response = Faraday.new(url: "#{SPOTIFY_AUTORES_API_URL}/#{autor.id_externo}/related-artists").get do |req|
       req.headers['Authorization'] = "Bearer #{obtener_token}"
+      @logger.debug "Pidiendo autores relacionados a #{SPOTIFY_AUTORES_API_URL}/#{autor.id_externo}/related-artists - headers: #{req.headers.to_json}"
     end
 
     if response.status == 200
-      @logger.debug "Autores relacionados obtenidos: #{response.body}"
-      relacionados = parsear_relacionados(response)
-      AutoresRelacionados.new(autor, relacionados)
+      obtener_autores_relacionados_de_respuesta(autor, response)
     else
       @logger.error "Error al obtener autores relacionados: #{response.body}"
     end
@@ -50,7 +49,14 @@ class AdaptadorSpotify
       req.headers['Authorization'] = "Basic #{auth_header}"
       req.headers['Content-Type'] = CONTENT_TYPE
       req.body = { grant_type: GRANT_TYPE }
+      @logger.debug "Pidiendo token a #{SPOTIFY_TOKEN_API_URL} - headers: #{req.headers.to_json} - body: #{req.body.to_json}"
     end
+  end
+
+  def obtener_autores_relacionados_de_respuesta(autor, response)
+    @logger.debug "Autores relacionados obtenidos: #{response.body}"
+    relacionados = parsear_relacionados(response)
+    AutoresRelacionados.new(autor, relacionados)
   end
 
   def parsear_relacionados(response)
