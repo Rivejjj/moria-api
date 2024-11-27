6e4c6087f6676711778e2d4af6168c5cfec0c5d9
IvanBotoshansky <ibotoshansky@fi.uba.ar>
2024-11-27 10:56:17 -0300

    En progreso: Escenario: US49:1 - Usuario agrega un podcast a su playlist

---
+++ features/playlist/agregar_podcast_a_playlist.feature
@@ -0,0 +1,9 @@
+# language: es
+Característica: Agregar podcast a playlist
+
+  @wip
+  Escenario: US49:1 - Usuario agrega un podcast a su playlist
+    Dado que existe un usuario "Kevin"
+    Y existe un podcast "JRE" con id 1
+    Cuando el usuario agrega el podcast con id 1 a su playlist
+    Entonces se agrega el podcast "JRE" a la playlist
---
+++ features/step_definitions/playlist/agregar_podcast_a_playlist_steps.rb
@@ -0,0 +1,20 @@
+Dado('existe un podcast {string} con id {int}') do |nombre_podcast, id|
+  repo_contenido = RepositorioContenido.new
+  autor = Autor.new('autor', '12345678')
+  RepositorioAutores.new.save(autor)
+  info_podcast = InformacionContenido.new(nombre_podcast, autor, 2020, 180, 'genero')
+  podcast = Podcast.new(info_podcast, id)
+  repo_contenido.save(podcast)
+end
+
+Cuando('el usuario agrega el podcast con id {int} a su playlist') do |id_podcast|
+  request_body = { 'id_contenido' => id_podcast }.to_json
+  @response = Faraday.post("/usuarios/#{ID_PLATAFORMA_PRUEBA}/playlist", request_body, { 'Content-Type' => 'application/json' })
+end
+
+Entonces('se agrega el podcast {string} a la playlist') do |nombre_podcast|
+  expect(@response.status).to eq(201)
+  repo_usuarios = RepositorioUsuarios.new
+  usuario = repo_usuarios.get_by_id_plataforma(ID_PLATAFORMA_PRUEBA)
+  expect(usuario.playlist.any? { |contenido| contenido.nombre == nombre_podcast }).to eq true
+end
